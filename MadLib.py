b1dd4cb19e847499fca3b056896fc07c20624622
Ryan Grissinger <rgrissinger@roofmaxx.com>
2021-05-22 13:43:41 -0400

    update MadLib to correct 2nd adjective variable

--- MadLib.py
+++ MadLib.py
@@ -4,10 +4,10 @@
 adjective = input("enter an adjective\n")
 noun = input("enter a noun\n")
 verb = input("enter a verb ending in past tense\n")
-adjective = input("enter an another adjective\n")
+adjective2 = input("enter an another adjective\n")
 color = input("enter a color\n")
 animal = input("enter an animal\n")
 
 # Desired result:
 #the big cat ran over the dumb white fish
-print("the", adjective, noun, verb, "over the", adjective, color, animal)
+print("the", adjective, noun, verb, "over the", adjective2, color, animal)

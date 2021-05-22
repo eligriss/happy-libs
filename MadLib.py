17b06604fef5c474b9a5f44714df84fa9bcd533a
Ryan Grissinger <ryan.grissinger@gmail.com>
2021-05-22 13:33:00 -0400

    add both madlibs

---
+++ MadLib.py
@@ -0,0 +1,13 @@
+# MadLib.py
+# this is from page 9
+# written by eli on 5/22/2021
+adjective = input("enter an adjective\n")
+noun = input("enter a noun\n")
+verb = input("enter a verb ending in past tense\n")
+adjective = input("enter an another adjective\n")
+color = input("enter a color\n")
+animal = input("enter an animal\n")
+
+# Desired result:
+#the big cat ran over the dumb white fish
+print("the", adjective, noun, verb, "over the", adjective, color, animal)
---
+++ MadLib2.py
@@ -0,0 +1,11 @@
+# MadLib.py
+# this is from page 9
+# written by eli on 5/22/2021
+adjective = input("enter a adjective\n")
+noun = input("enter a noun\n")
+verb = input("enter a verb ending in -ed\n")
+
+# Desired result:
+# the small scooby doo walked over the lazy brown dog.
+# the  scooby doo small walked over the lazy brown dog.
+print("the", adjective, noun, verb, "over the lazy brown dog.")
